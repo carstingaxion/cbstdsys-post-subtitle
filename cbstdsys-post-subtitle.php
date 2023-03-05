cdc5a84cd178023e07f19c8d1c31b074d53f4063
carstenbach <mail@carsten-bach.de>
2023-03-05 15:17:35 +0100

    Bump version to 0.1.1

--- cbstdsys-post-subtitle.php
+++ cbstdsys-post-subtitle.php
@@ -4,7 +4,7 @@
  * Description:       Block to show and style a semantic subtitle – build step required.
  * Requires at least: 5.8
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            Carsten Bach
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
